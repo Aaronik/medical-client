9456cff20f555bc1a253844ec060b128a0a787c9
Aaron Sullivan <aasullivan1618@gmail.com>
2019-11-17 11:49:34 -0800

    Fix issue with calendar initially appearing blank
    Closes #55

--- src/common/components/DatePickerModal.tsx
+++ src/common/components/DatePickerModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import InfiniteCalendar from 'react-infinite-calendar'
 import 'react-infinite-calendar/styles.css'
@@ -16,10 +16,26 @@ const DatePickerModal: React.FC<TDatePickerModalProps> = ({ show, onSelect, onCl
     onClose()
   }
 
+  // There's an issue with the calendar initially appearing blank and back 50 years. (GH #55)
+  // This fixes that by finding the date that's selected and scrolling that to the center.
+  // I'm not sure Cal__Day__selection is a public API so this might not be a totally
+  // robust solution.
+  useEffect(() => {
+    const currentDaySelection = document.querySelector('.Cal__Day__selection')
+    if (!currentDaySelection) return
+    currentDaySelection.scrollIntoView({ block: 'center' })
+  }, [show])
+
   return (
     <Modal show={show} centered onHide={onClose}>
       <Modal.Body>
-        <InfiniteCalendar width='100%' onSelect={onCalendarSelect} selected={initialDate}/>
+        <InfiniteCalendar
+          width='100%'
+          onSelect={onCalendarSelect}
+          selected={initialDate}
+          autoFocus
+          display='days'
+          onScroll={() => {}}/>
       </Modal.Body>
     </Modal>
   )
