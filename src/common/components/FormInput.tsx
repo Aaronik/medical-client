43428327c9a783a94ce6c6d8a30b4c5bca597497
Aaron Sullivan <aasullivan1618@gmail.com>
2019-12-16 13:21:31 -0800

    Fix FormInput autoFocus
    * Didn't work before here due to bug in React-Bootstrap
    * This docs that and fixes it manually.

--- src/common/components/FormInput.tsx
+++ src/common/components/FormInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -19,21 +19,31 @@ type TProps = {
 const deconstructEvent = (onChange: Function) => (event: any) => onChange(event.currentTarget.value)
 
 // This component is here to abstract common input fields.
-// ATTOW this was used in signin and signout.
-// Ideally, if more pages are built, this same style will be used
-// on inputs and this can be used in more places.
-const FormInput: React.FunctionComponent<TProps> = ({ label, type, onChange, value, icon, onIconClick, autoFocus }) => (
-  <Form.Group className='custom-form-group'>
-    <Form.Label className="text-muted">{label}</Form.Label>
-    <InputGroup>
-      <Form.Control autoFocus={autoFocus} type={type} onChange={deconstructEvent(onChange)} value={value}></Form.Control>
-      { icon &&
-        <InputGroup.Append onClick={onIconClick} style={{ cursor: onIconClick ? "pointer" : "default" }}>
-          <InputGroup.Text><FontAwesomeIcon icon={icon} /></InputGroup.Text>
-        </InputGroup.Append>
-      }
-    </InputGroup>
-  </Form.Group>
-)
+const FormInput: React.FunctionComponent<TProps> = ({ label, type, onChange, value, icon, onIconClick, autoFocus }) => {
+
+  // Have to use "any" as there's no FormControl export:
+  // https://github.com/react-bootstrap/react-bootstrap/issues/3568
+  const formControlEl = useRef<any>(null)
+
+  // We have to manually autofocus because, for some reason, the build in autoFocus flag is not working:
+  // https://github.com/react-bootstrap/react-bootstrap/issues/429
+  useEffect(() => {
+    if (autoFocus) formControlEl.current.focus()
+  }, [autoFocus])
+
+  return (
+    <Form.Group className='custom-form-group'>
+      <Form.Label className="text-muted">{label}</Form.Label>
+      <InputGroup>
+        <Form.Control ref={formControlEl} type={type} onChange={deconstructEvent(onChange)} value={value}></Form.Control>
+        { icon &&
+          <InputGroup.Append onClick={onIconClick} style={{ cursor: onIconClick ? "pointer" : "default" }}>
+            <InputGroup.Text><FontAwesomeIcon icon={icon} /></InputGroup.Text>
+          </InputGroup.Append>
+        }
+      </InputGroup>
+    </Form.Group>
+  )
+}
 
 export default FormInput
