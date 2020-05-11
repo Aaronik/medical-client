52b4dadf5a1e99f0f1acb8772dc39ff4a5971eec
Aaron Sullivan <aasullivan1618@gmail.com>
2020-05-11 13:14:16 -0700

    Small changes to Doctor Dashboard. Still needs work.

--- src/pages/DoctorDashboard.tsx
+++ src/pages/DoctorDashboard.tsx
@@ -7,7 +7,7 @@ import Avatar from 'components/Avatar'
 import UpdateCard from 'components/UpdateCard'
 import { TUser } from 'types/User.d'
 import { TNotification } from 'types/Notification.d'
-import formatDate from 'util/formatDate'
+/* import formatDate from 'util/formatDate' */
 import strings from './DoctorDashboard.strings'
 import { useSetActivePatient } from 'util/hooks'
 
@@ -15,67 +15,6 @@ interface TProps {
   patients: TUser[]
   user: TUser
   notifications: TNotification[]
-}
-
-const NumberBadge: React.FC<{ bg: string }> = ({ bg, children }) => {
-  const style = {
-    width: '50px',
-    borderRadius: '20px',
-  } as const
-
-  return <div style={style} className={'p-3 m-2 text-center bg-' + bg}>{children}</div>
-}
-
-const PatientCard: React.FC<{ patient: TUser }> = ({ patient }) => {
-  const { name, birthday, joinDate, lastVisit, adherence } = patient
-
-  const [ setActiveP ] = useSetActivePatient()
-
-  const containerStyle = {
-    cursor: 'pointer',
-    borderRadius: '20px',
-    width: '350px',
-  } as const
-
-  const onCardClick = () => setActiveP(patient.id, '/overview')
-
-  return (
-    <div style={containerStyle} className='bg-white p-3 m-3' onClick={onCardClick}>
-
-      <Row className='justify-content-center mb-5'>
-        <div className='flex-column'>
-          <Avatar user={patient} size={80} onClick={onCardClick}/>
-          <div>{name}</div>
-          <div className='text-muted text-center'>{birthday ? formatDate(birthday) : strings('na')}</div>
-        </div>
-      </Row>
-
-      <Row>
-          <Col>
-            <div className='text-muted badge'>{strings('joined')}</div>
-            <div className='text-center font-weight-bolder'>{ joinDate ? formatDate(joinDate) : strings('na')}</div>
-          </Col>
-
-          <Col>
-            <div className='text-muted badge'>{strings('lastVisit')}</div>
-            <div className='text-center font-weight-bolder'>{lastVisit ? formatDate(lastVisit) : strings('na')}</div>
-          </Col>
-
-          <Col>
-            <div className='text-muted badge'>{strings('adherence')}</div>
-            <div className='text-center font-weight-bolder'>{adherence ? adherence + "%" : strings('na')}</div>
-          </Col>
-      </Row>
-
-      <Row className='justify-content-around'>
-        <NumberBadge bg='danger'>11</NumberBadge>
-        <NumberBadge bg='success'>89</NumberBadge>
-        <NumberBadge bg='pink'>1</NumberBadge>
-        <NumberBadge bg='warning'>12</NumberBadge>
-      </Row>
-
-    </div>
-  )
 }
 
 const DoctorDashboard: React.FunctionComponent<TProps> = ({ patients, user, notifications }) => {
@@ -104,6 +43,71 @@ const DoctorDashboard: React.FunctionComponent<TProps> = ({ patients, user, noti
       </Row>
     </Container>
   )
+}
+
+const PatientCard: React.FC<{ patient: TUser }> = ({ patient }) => {
+  const { name, birthday, joinDate, lastVisit, adherence } = patient
+
+  const [ setActiveP ] = useSetActivePatient()
+
+  const containerStyle = {
+    cursor: 'pointer',
+    borderRadius: '20px',
+    width: '350px',
+  } as const
+
+  const onCardClick = () => setActiveP(patient.id, '/overview')
+
+  return (
+    <div style={containerStyle} className='bg-white p-3 m-3' onClick={onCardClick}>
+
+      <Row className='justify-content-center mb-5'>
+        <div className='flex-column'>
+          <Avatar user={patient} size={80} onClick={onCardClick}/>
+          <div>{name}</div>
+          <div className='text-muted text-center'>{birthday ? formatDate(birthday) : strings('na')}</div>
+        </div>
+      </Row>
+
+      <Row>
+        <Col>
+          <div className='text-muted badge'>{strings('joined')}</div>
+          <div className='text-center font-weight-bolder'>{ joinDate ? formatDate(joinDate) : strings('na')}</div>
+        </Col>
+
+        <Col>
+          <div className='text-muted badge'>{strings('lastVisit')}</div>
+          <div className='text-center font-weight-bolder'>{lastVisit ? formatDate(lastVisit) : strings('na')}</div>
+        </Col>
+
+        <Col>
+          <div className='text-muted badge'>{strings('adherence')}</div>
+          <div className='text-center font-weight-bolder'>{adherence ? adherence + "%" : strings('na')}</div>
+        </Col>
+      </Row>
+
+      <Row className='justify-content-around'>
+        <NumberBadge bg='danger'>11</NumberBadge>
+        <NumberBadge bg='success'>89</NumberBadge>
+        <NumberBadge bg='pink'>1</NumberBadge>
+        <NumberBadge bg='warning'>12</NumberBadge>
+      </Row>
+
+    </div>
+  )
+}
+
+const NumberBadge: React.FC<{ bg: string }> = ({ bg, children }) => {
+  const style = {
+    width: '50px',
+    borderRadius: '20px',
+  } as const
+
+  return <div style={style} className={'p-3 m-2 text-center bg-' + bg}>{children}</div>
+}
+
+const formatDate = (date: Date | number | string) => {
+  return (new Date(+date)).toLocaleDateString()
 }
 
 export default DoctorDashboard
